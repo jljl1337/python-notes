94201b35a91ad4d3740a882918004afd71335c04
jljl1337 <lckjack123@gmail.com>
2020-08-24 20:13:41 +0800

    Added the conversion of type of variables

--- notes.py
+++ notes.py
@@ -1,16 +1,28 @@
-# print statement
+# comment
+# either start at the beginning of the line
+# or TWO space after any lines
 
+# print statement
 print("Hello world")  # this output the string "Hello world"
 print("*" * 3)  # this output "***"
 
-# variables (MUST INOT* CAP THE FIRST LETTER)
 
+# variables (MUST INOT* CAP THE FIRST LETTER)
 price = 10  # integer
 rating = 4.9  # float
 full_name = 'Jack'  # string
 iamgreat = True  # Boolean (MUST CAP THE FIRST LETTER)
 
+
 # input statement
-name = input("Your name is: ")
+name = input('Your name is: ')  # must get a string
 colour = input("Your favourite colour is: ")
 print(name + " like " + colour + '.')
+
+
+# conversion of type of variables
+g_lbs = input('weight in lbs: ')
+g_kg = int(g_lbs) * 0.45  # changing the g_lbs into float first
+print(g_kg)
+print(type(g_kg))  # output the type of g_kg, which is float
+print(type(str(g_kg)))  # changeing the type of g_kg into string
