c542575f0a1aa7af8c8ca33227cbe636114b6dac
jljl1337 <lckjack123@gmail.com>
2020-08-24 19:42:33 +0800

    Added variables and input statement

--- notes.py
+++ notes.py
@@ -1,3 +1,16 @@
 # print statement
-print("Hello world")  # this output the srtring "Hello world"
+
+print("Hello world")  # this output the string "Hello world"
 print("*" * 3)  # this output "***"
+
+# variables (MUST INOT* CAP THE FIRST LETTER)
+
+price = 10  # integer
+rating = 4.9  # float
+name = 'Jack'  # string
+iamgreat = True  # Boolean (MUST CAP THE FIRST LETTER)
+
+# input statement
+name = input("Your name is: ")
+colour = input("Your favourite colour is: ")
+print(name + " like " + colour + '.')
