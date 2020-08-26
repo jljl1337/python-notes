1a29b25045f19e50b5950f6f572dd0789c74a6b2
jljl1337 <lckjack123@gmail.com>
2020-08-26 15:32:01 +0800

    Format

--- notes.py
+++ notes.py
@@ -115,6 +115,7 @@ if len(name) == 0:
     print("please enter the name")
 elif name != "Jack Leung":
     print("")
+
 if len(name) < 3:
     print("the name must be at least 3 characters long")
 elif len(name) > 50:
